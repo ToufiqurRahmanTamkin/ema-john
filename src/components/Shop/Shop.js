2179a30141cc16e85b0145ea585efefd61a144b9
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-03-04 19:16:44 +0600

    store data in local storage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -10,9 +11,12 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) =>{
-        console.log('product added', product);
+        // console.log('product added', product);
         const newCart = [...cart,product];
         setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length;
+        addToDatabaseCart(product.key, count)
     }
 
     return (
@@ -22,6 +26,7 @@ const Shop = () => {
                 {
                     products.map(pd =>
                     <Product 
+                        key={pd.key}
                         handleAddProduct = {handleAddProduct}
                         product ={pd}
                         showAddToCart = {true}>
